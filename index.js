62cb37ae3f36da1567ea44725393d9283edae87e
Nguyen Duc Vi <nducvi.information@gmail.com>
2025-02-22 16:39:51 +0700

    add button

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// tự động zoom 180% với kích thước màn lớn
 if (window.innerWidth >= 1024) { // Chỉ áp dụng trên máy tính
-    document.body.style.zoom = "160%";
+    document.body.style.zoom = "180%";
 }
+
+// nút nhảy xuống ảnh photobooth
+document.getElementById("scrollToGallery").addEventListener("click", function() {
+    document.getElementById("gallery").scrollIntoView({ behavior: "smooth" });
+});
+
+// Nút quay lên đầu trang
+document.getElementById("scrollToTop").addEventListener("click", function() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
